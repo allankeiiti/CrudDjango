f111c51de0e91af0fea3eb7552bcc9df9a9ae1f8
allan.nakakita <allan.keiiti@gmail.com>
2019-09-17 10:21:50 -0300

    ... - Criado html de listagem o qual tem o jinja implementado - Mexido nas URLs para abrir o home digitando somente a URL - Criado Models de Categoria e Transação

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    tipo_categoria = models.IntegerField(primary_key = True)
+    nome_categoria = models.CharField(max_length = 100)
+    dt_criacao = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.nome_categoria
+
+class Transacao(models.Model):
+    dt_transacao = models.DateTimeField()
+    descricao = models.CharField(max_length = 200)
+    valor = models.DecimalField(max_digits = 7, decimal_places = 2)
+    Categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+    observacoes = models.TextField(null = True, blank = True)
+
+    # Alterando como fica o display da classe nos Fields
+    class Meta:
+        verbose_name_plural = 'Transações'
+
+    def __str__(self):
+        return self.descricao
--- contas/views.py
+++ contas/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 import datetime
+from contas.models import Transacao, Categoria
 
 def home(request):
-    now = datetime.datetime.now()
+
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+
+    data['now'] = datetime.datetime.now()
     # html = '<html><title>DateTime</title><body>Agora é %s.</body></html>' % now
     # return HttpResponse(html)
-    return render(request, 'contas/home.html')
+    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    data['categorias'] = Categoria.objects.all()
+    return render(request, 'contas/listagem.html', data)
